4b8057c23d545fafeb1db778fd6e52d884ace46b
Rakshitha N M <rakshitha9870@gmail.com>
2026-01-30 01:08:36 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,7 @@
+class StringLength {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int l = str.length();  
+        System.out.println("Length of the string: " + l);
+    }
+}
