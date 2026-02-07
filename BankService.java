90a3420528acde99ee884dfa37747f4d9fd730a3
Rakshitha N M <rakshitha9870@gmail.com>
2026-02-07 14:13:10 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -41,8 +41,8 @@ public class BankService {
        acc.deposit(700);
        System.out.println("Balance after deposit of 700: " + acc.getBalance());
  
-       acc.withdraw(300);
-       System.out.println("Balance after withdrawal of 300: " + acc.getBalance());
+       acc.withdraw(400);
+       System.out.println("Balance after withdrawal of 400: " + acc.getBalance());
  
        // Print final result
        System.out.println("Final Balance: " + acc.getBalance());
