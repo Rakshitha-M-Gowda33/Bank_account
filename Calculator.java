be813c27b10127347a1f2e842a26e3cb4cf3a6da
Rakshitha N M <rakshitha9870@gmail.com>
2026-04-22 15:22:33 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,8 @@
-package com.example;
+package Project;
+
 
 public class Calculator {
-
-    public int add(int a, int b) {
+	public int add(int a, int b) {
         return a + b;
     }
 
@@ -15,10 +15,23 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
-        if(b==0){
-            System.out.println("deviosn by zaro is not allowed");
+    	if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
             return 0;
-        }
+    	}
         return a / b;
     }
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        int a = 6;
+        int b = 3;
+
+        System.out.println("Addition: " + calc.add(a, b));
+        System.out.println("Subtraction: " + calc.subtract(a, b));
+        System.out.println("Multiplication: " + calc.multiply(a, b));
+        System.out.println("Division: " + calc.divide(a, b));
+    }
+
 }
